1ad25de7fc9443e752fc1135e4e2af5ae021a085
Alumno <alumno@laboratorios-macbook-pro-7.local>
2019-03-29 15:51:50 -0600

    Pod Firebase

--- PrecioTec/AppDelegate.swift
+++ PrecioTec/AppDelegate.swift
@@ -17,6 +17,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication,
                      didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?)-> Bool {
             FirebaseApp.configure()
+        let myDatabase = Database.database().reference()
+        myDatabase.setValue("Datos")
             return true
     }
 }
