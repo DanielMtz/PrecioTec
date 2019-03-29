b1c3a6c451ce9067a5498f2d2be7071b843dac3c
Alumno <alumno@laboratorios-macbook-pro-7.local>
2019-03-29 14:59:26 -0600

    Initial Commit

---
+++ PrecioTec/DetailViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PrecioTec
+//
+//  Created by Alumno on 3/26/19.
+//  Copyright © 2019 Tec de Monterrey. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
