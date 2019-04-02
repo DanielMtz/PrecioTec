ed8671f035b13ca6eaeb0dae6214edfc4959b560
Alumno <alumno@laboratorios-macbook-pro-7.local>
2019-04-02 15:17:54 -0600

    Ajuste TableViewCell

--- PrecioTec/DetailViewController.swift
+++ PrecioTec/DetailViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 import Firebase
 import SVProgressHUD
 
+
+
 class DetailViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
     var metasArray = [Metas]()
